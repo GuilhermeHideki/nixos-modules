d8d1a9c2c36e9d1310212057ce5309d138cf6ecc
Guilherme Danno <guilherme.danno@gmail.com>
2025-05-09 01:28:19 -0300

    chore: template flake.nix

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "My public modules";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosModules = {
+    };
+  };
+}

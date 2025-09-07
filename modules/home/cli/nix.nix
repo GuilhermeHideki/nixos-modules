2542da8c7fa4844aca5aa6b596b008ae1e047a90
Guilherme Danno <guilherme.danno@gmail.com>
2025-09-07 01:25:44 -0300

    feat: configure PROJECT_ROOTS env var

--- modules/home/cli/nix.nix
+++ modules/home/cli/nix.nix
@@ -6,13 +6,31 @@
   pkgs,
   namespace,
   ...
-}: {
+}: let
+  cfg = config.${namespace};
+in {
+  options.${namespace} = {
+    projectRoots = lib.mkOption {
+      description = ''
+        Where should I search for projects
+      '';
+      type = lib.types.listOf lib.types.str;
+      default = [];
+    };
+  };
+
   config = {
     ${namespace}.shell.aliases = {
       hms = "nh home switch $NIXOS_REPO";
       nup = "nh os switch $NIXOS_REPO";
       nupp = "nh os switch $NIXOS_REPO --update";
       nfu = "nix flake update";
+    };
+
+    home.homeDirectory = "/home/${config.home.username}";
+
+    home.sessionVariables = {
+      PROJECT_ROOTS = lib.strings.concatStringsSep ":" cfg.projectRoots;
     };
 
     home.packages = with pkgs; [
